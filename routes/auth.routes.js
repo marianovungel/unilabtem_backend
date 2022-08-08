42bf5d44de99b664b04415f9e05b42cb08cbf689
marianovungel <vungem88@gmail.com>
2022-08-08 01:40:51 -0300

    validação registrar

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -17,9 +17,9 @@ router.post("/register", async(req, res)=>{
         });
 
         const user = await nerUser.save();
-        res.status(200).json(user);
+        return res.status(200).json(user);
     }catch(err){
-        res.status(500).json(err);
+        res.status(400).json(err);
     }
     
 });
