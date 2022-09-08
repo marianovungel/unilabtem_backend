2a5da6a5339df3006b5c1002574f0039d9e3fda0
marianovungel <vungem88@gmail.com>
2022-09-08 14:59:39 -0300

    null

--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
     },
     profilePic:{
         type:String,
-        default: "",
+        default: null,
     },
     whatsapp: {
         type:String,
