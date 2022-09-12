ec78b862e8af46ca0a47efb30e73f1b5e72164d8
marianovungel <vungem88@gmail.com>
2022-09-12 00:52:37 -0300

    new: true in update user

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -5,6 +5,24 @@ const bcrypt = require("bcrypt")
 
 
 //Update user
+// router.put('/:id', async (req, res) => {
+//     const id = req.params.id;
+//     const novo_user = req.body;
+//     if(novo_user.userId === id){
+//         if(req.body.password){
+//                 const salt = await bcrypt.genSalt(10);
+//                 novo_user.password = await bcrypt.hash(req.body.password, salt);
+//         }
+//         try{
+//             const updateUser = await User.findByIdAndUpdate(id, novo_user);
+//             res.json({error: false, updateUser});
+//         }catch(err){
+//             res.json({error: true, message: err.message});
+//         }
+//     }else{
+//         res.status(401).json("tu só podes atualizar a sua conta");
+//     }
+// })
 router.put('/:id', async (req, res) => {
     const id = req.params.id;
     const novo_user = req.body;
@@ -14,7 +32,9 @@ router.put('/:id', async (req, res) => {
                 novo_user.password = await bcrypt.hash(req.body.password, salt);
         }
         try{
-            const updateUser = await User.findByIdAndUpdate(id, novo_user);
+            const updateUser = await User.findByIdAndUpdate(id, {
+                $set: novo_user
+            }, {new:true});
             res.json({error: false, updateUser});
         }catch(err){
             res.json({error: true, message: err.message});
