047adecfc1267c38ca246e4faedd7d649415e217
marianovungel <vungem88@gmail.com>
2022-10-28 16:47:53 -0300

    Confirmação de email

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,21 +5,55 @@ const bcrypt = require("bcrypt")
 const nodemailer = require("nodemailer")
 var jwt = require('jsonwebtoken');
 
-router.post("/register", async(req, res)=>{
-    try{
-        //usando bcrypt para incriptar a senha
+
+const verifyTokenUser = (req, res, next)=>{
+    const token = req.body.path;
+    if(token){
+        jwt.verify(token, process.env.JWT_SEC, (err, user)=>{
+            if(err) res.status(403).json("Token inválido!")
+            req.user=user;
+            next();
+        })
+    }else{
+        return res.status(401).json("Usuário Não autenticado!")
+    }
+}
+var AuthUser;
+const verifyTokenAndAuthorizationUser =  (req, res, next)=>{
+    verifyTokenUser(req, res, async ()=>{
+        // var produtId = await Produto.findById(req.params.id)
         const salt = await bcrypt.genSalt(10);
-        const hashedPass = await bcrypt.hash(req.body.password, salt)
+        const hashedPass = await bcrypt.hash(req.user.password, salt)
         const nerUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            whatsapp: req.body.whatsapp,
+            username: req.user.username,
+            email: req.user.to,
+            whatsapp: req.user.whatsapp,
             password: hashedPass,
             profilePic: "74d5d28e4db58837d16d30eb57d8e8e6"
         });
+        AuthUser = await nerUser.save();
+        if(req.user){
+            next();
+        }else{
+            return res.status(403).json("Deixe de Roubo!")
+        }
+    })
+}
+router.post("/register", verifyTokenAndAuthorizationUser, async(req, res)=>{
+    try{
+        //usando bcrypt para incriptar a senha
+        // const salt = await bcrypt.genSalt(10);
+        // const hashedPass = await bcrypt.hash(req.body.password, salt)
+        // const nerUser = new User({
+        //     username: req.body.username,
+        //     email: req.body.email,
+        //     whatsapp: req.body.whatsapp,
+        //     password: hashedPass,
+        //     profilePic: "74d5d28e4db58837d16d30eb57d8e8e6"
+        // });
 
-        const user = await nerUser.save();
-        return res.status(200).json(user);
+        // const user = await nerUser.save();
+        return res.status(200).json({AuthUser});
     }catch(err){
         res.status(400).json(err);
     }
@@ -69,6 +103,59 @@ router.post("/sendemail", async(req, res)=>{
             var to = req.body.to
             var subject = "Reset Password"
             var message = `Olá ${user.username}! Este é o código de verificação do seu email para poder alterar a sua palavra passe. codigo: ${codigo}`
+
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                user: 'unilabtem@gmail.com',
+                pass: 'xruetlpwtoquvnye'
+                }
+            })
+
+            var mailOptions = {
+                from: from,
+                to:to,
+                subject:subject,
+                text:message
+            }
+
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    return status(500).json("Algo deu errado no envio do email...");
+                } else {
+                    console.log("Email Sent: " + info.response)
+                  return  res.status(200).json(codigo);
+                }
+                // response.redirect("/")
+            })
+        }
+        return res.status(200).json(user);
+    }catch(err){
+        res.status(500).json(err);
+    }
+    
+});
+router.post("/sendemailtoconfirm", async(req, res)=>{
+    try{
+        const user = req.body;
+
+        if(!user){
+            return res.status(400).json("Não existe usuário cadastrado com este email!");
+        }
+        if(user){
+
+            const accessTokenAcount = jwt.sign({
+                username: req.body.username,
+                whatsapp: req.body.whatsapp,
+                password: req.body.password,
+                to: req.body.to,
+              }, process.env.JWT_SEC)
+            
+            var codigo = req.body.codigo
+            var from = req.body.from
+            var to = req.body.to
+            var subject = "Confirmação de E-mail"
+            var message = `Olá ${user.username}! confirma a sua conta de e-mail para poder criar a sua conta na plataforma Unilabtem clicando na URL : http://localhost:3000/conf/${accessTokenAcount}`
 
             var transporter = nodemailer.createTransport({
                 service: 'gmail',
